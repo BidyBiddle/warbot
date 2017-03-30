8025010df882d46f439bb8219f7dfe48b413d6fc
Benoit Lery <biddle@urban-zone.org>
2017-03-30 20:58:01 +0200

    pircbot core

---
+++ org/pircbotx/cap/EnableCapHandler.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (C) 2010-2013 Leon Blakey <lord.quackstar at gmail.com>
+ *
+ * This file is part of PircBotX.
+ *
+ * PircBotX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PircBotX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PircBotX. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.pircbotx.cap;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.pircbotx.PircBotX;
+import org.pircbotx.exception.CAPException;
+
+/**
+ * Enables the specified capability with the server. This handler should cover
+ * almost all CAP features except SASL since most only need to be requested.
+ * @author Leon Blakey <lord.quackstar at gmail.com>
+ */
+@RequiredArgsConstructor
+public class EnableCapHandler implements CapHandler {
+	@Getter
+	protected boolean done = false;
+	@Getter
+	protected final String cap;
+	protected final boolean ignoreFail;
+
+	/**
+	 * Create EnableCapHandler not ignoring if server doesn't support the requested
+	 * capability
+	 */
+	public EnableCapHandler(String cap) {
+		this.cap = cap;
+		this.ignoreFail = false;
+	}
+
+	public void handleLS(PircBotX bot, List<String> capabilities) throws CAPException {
+		if (capabilities.contains(cap))
+			//Server supports sasl, send request to use it
+			bot.sendCAPREQ(cap);
+		else if (!ignoreFail)
+			throw new CAPException("Server does not support the " + cap + " capability");
+		else
+			//Nothing more to do
+			done = true;
+	}
+
+	public void handleACK(PircBotX bot, List<String> capabilities) throws CAPException {
+		if (capabilities.contains(cap))
+			//Capability is now enabled
+			done = true;
+	}
+
+	public void handleNAK(PircBotX bot, List<String> capabilities) throws CAPException {
+		if (capabilities.contains(cap)) {
+			//Make sure the bot didn't register this capability
+			bot.getEnabledCapabilities().remove(cap);
+			if (!ignoreFail)
+				throw new CAPException("Server does not support the " + cap + " capability");
+			else
+				//Nothing more to do
+				done = true;
+		}
+	}
+
+	public void handleUnknown(PircBotX bot, String rawLine) {
+	}
+}
---
+++ org/pircbotx/hooks/WaitForQueue.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (C) 2010-2013 Leon Blakey <lord.quackstar at gmail.com>
+ *
+ * This file is part of PircBotX.
+ *
+ * PircBotX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PircBotX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PircBotX. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.pircbotx.hooks;
+
+import java.io.Closeable;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import org.pircbotx.PircBotX;
+
+/**
+ * Stores all events in a queue for processing. This is useful for sequential 
+ * processing of many similar events.
+ * <p>
+ * Example:
+ * <code>
+ * WaitForQueue queue = new WaitForQueue();
+ * while(true) {
+ *     MessageEvent mevent = queue.waitFor(MessageEvent.class);
+ *     //Process event
+ * }
+ * queue.done();
+ * </code>
+ * @author Leon Blakey <lord.quackstar at gmail.com>
+ */
+public class WaitForQueue implements Closeable {
+	protected final PircBotX bot;
+	protected BlockingQueue<Event> eventQueue = new LinkedBlockingQueue<Event>();
+	protected WaitForQueueListener listener;
+
+	/**
+	 * Create and store a queue listener in the specified bot's ListenerManager.
+	 * It will be removed when {@link #done()} is called
+	 * @param bot 
+	 */
+	public WaitForQueue(PircBotX bot) {
+		this.bot = bot;
+		bot.getListenerManager().addListener(listener = new WaitForQueueListener());
+	}
+
+	/**
+	 * Wait for events of the specified event class to appear in the queue. If 
+	 * the event was dispatched before this is called, it will return immediately. 
+	 * Events that do not match the specified event class are discarded
+	 * @param <E>
+	 * @param eventClass
+	 * @return
+	 * @throws InterruptedException 
+	 */
+	public <E extends Event> E waitFor(Class<E> eventClass) throws InterruptedException {
+		if (eventClass == null)
+			throw new IllegalArgumentException("Can't wait for null event");
+		while (true) {
+			Event curEvent = eventQueue.take();
+			if (eventClass.isInstance(curEvent))
+				return (E) curEvent;
+		}
+	}
+
+	/**
+	 * Shuts down the queue; VERY important to call when finished. Since this class 
+	 * stores every dispatched event, failure to close will eventually cause you
+	 * to run out of memory
+	 */
+	@Override
+	public void close() {
+		bot.getListenerManager().removeListener(listener);
+		eventQueue.clear();
+	}
+
+	protected class WaitForQueueListener implements Listener {
+		public void onEvent(Event event) throws Exception {
+			eventQueue.add(event);
+		}
+	}
+}
